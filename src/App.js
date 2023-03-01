df6dea97207b8754a5b64e71f26f50a9c4167de7
Jocelle Bautista <jcllebautista@gmail.com>
2023-02-28 21:24:35 -0500

    Install React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Breweries } from "./displays/Breweries";
+import { Search } from "./displays/Search";
+import { BrewLog } from "./displays/BrewLog";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/brewlog">BrewLog</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home" element={<Breweries />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/brewlog" element={<BrewLog />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
