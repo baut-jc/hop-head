58e5b20ad375f697f13acb22fb20e0ae7a322a8d
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-01 17:59:53 -0500

    Add hooks to breweries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from "react";
+import { 
+  React,
+  useState
+ } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
+import { fetchData } from "./api";
 import { Breweries } from "./displays/Breweries";
 import { Search } from "./displays/Search";
 import { BrewLog } from "./displays/BrewLogs";
+import './App.css'
+
 
 
 export default function App() {
+
+  const [breweries, setBreweries] = useState([])
+
   return (
     <Router>
       <div>
         <nav>
           <ul>
             <li>
-              <Link to="/home">Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
               <Link to="/search">Search</Link>
@@ -28,7 +37,7 @@ export default function App() {
           </ul>
         </nav>
         <Routes>
-          <Route path="/home" element={<Breweries />} />
+          <Route path="/" element={<Breweries />} />
           <Route path="/search" element={<Search />} />
           <Route path="/brewlog" element={<BrewLog />} />
         </Routes>
--- src/components/BreweryDetails/BreweryDetails.js
+++ src/components/BreweryDetails/BreweryDetails.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import { 
+  React,
+  useState 
+} from 'react'
 
 export const BreweryDetails = () => {
+
+  const [brewery, setBrewery] = useState('')
+  const [address, setAddress] = useState('')
+  const [contact, setContact] = useState('')
+  const [website, setWebsite] = useState('')
+
+
   return (
     <>
       <h1>BreweryName</h1>
       <p>address</p>
       <p>phoneNumber</p>
-      <p>hours</p>
       <a>weblink</a>
     </>
   )
