10b27fedc2110e052039e7c2ca7dc59bf3e4372a
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-02 17:27:00 -0500

    Add param to make API endpoint dynamic

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const fetchData = () => {
-  return (fetch('https://api.openbrewerydb.org/breweries/')
+const fetchData = (zipCode) => {
+  return (fetch(`https://api.openbrewerydb.org/breweries?by_postal=${zipCode}`)
   .then(response => {
     console.log('response', response)
     if(response.ok) {
@@ -7,6 +7,8 @@ const fetchData = () => {
     } else {
       throw new Error(`${response.status} ${response.statusText}`)
     }
-  }))}
+  }))
+}
 
-  export default fetchData
+
+  export { fetchData }
