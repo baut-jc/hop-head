448d31c255041fff10df033e382367e262ba31dd
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-02 18:45:29 -0500

    Add requirement for form

--- src/displays/Search.js
+++ src/displays/Search.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Form } from 'react-router-dom'
 
-export const Search = () => {
+export function Search({takeZipCode}) {
+  
+  const [postalCode, setPostalCode] = useState('')
+
+  const submitForm = event => {
+    event.preventDefault()
+    takeZipCode(`${postalCode}`)
+    clearForm()
+  }
+  
+  const clearForm = () => {
+    setPostalCode('')
+  }
+
   return (
     <div className='zip-form'>
-      <form>
-        <input type='text' placeholder='Enter ZIP'/>
-        <Link to='/'>
-          <button>Search</button>
-        </Link>
+      <form onSubmit={submitForm}>
+        <input 
+          name='zipCode'
+          type='number'
+          min='00501'
+          max='99950'
+          value={postalCode}
+          placeholder='Enter ZIP'
+          onChange={(e) => setPostalCode(e.target.value)}
+          required 
+          />
+          <button>Cheers</button> {/* cheers sound*/}
+        {/* <Link to='/'> */}
+        {/* </Link> */}
       </form>
     </div>
   )
