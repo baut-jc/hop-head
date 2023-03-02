65a67a7d308e7e81ca3dd19f87ae00c8e6ea3916
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-01 23:50:25 -0500

    Render brewery card to breweries page

--- src/displays/Breweries.js
+++ src/displays/Breweries.js
@@ -1,13 +1,43 @@
-import React from 'react'
-import { BreweryDetails } from '../components/BreweryDetails/BreweryDetails'
+import React, { useState, useEffect } from 'react';
+import fetchData from '../api'
+import { BreweryDetails } from '../components/BreweryDetails/BreweryDetail'
 
 export const Breweries = () => {
-  // const [breweries, setBreweries] = useState([])
+  const [breweries, setBreweries] = useState([])
+  const [networkError, setNetworkError] = useState(false)
+
+  const fetchBreweries = async () => {
+    try {
+      const data = await fetchData()
+      setBreweries(data)
+      console.log('breweries' , breweries)
+      setNetworkError(false)
+    } catch (error) {
+      console.error(error)
+      setNetworkError(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchBreweries()
+  }, [])
+
+  const breweryCards = breweries.map(brewery => {
+    console.log('brewery', brewery)
+    return (
+        <BreweryDetails 
+          id={brewery.id}
+          // name={brewery.name}
+          // street={brewery.street}
+          // contact={brewery.contact}
+          // website={brewery.website}
+          />
+    )
+  })
 
   return (
-    <div>
-      <title>Breweries</title>
-      <BreweryDetails />
-      </div>
+    <div className='breweries-display'>
+      {breweryCards}
+    </div>
   )
 }
