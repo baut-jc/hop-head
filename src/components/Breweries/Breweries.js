73b0bb3825f6fbfdb1071235ceb6d2b38b46b427
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-06 09:28:36 -0500

    Add prop types to Breweries

--- src/components/Breweries/Breweries.js
+++ src/components/Breweries/Breweries.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { fetchData } from '../../api'
 import BreweryDetails from '../BreweryDetails/BreweryDetail'
-import { Error } from '../Errors/error';
+import { Error } from '../Errors/error'
 import './Breweries.css'
+import PropTypes from 'prop-types'
 
 export const Breweries = ({zipCode, addFaveBreweries, faveBreweries, unFaveBrewery}) => {
   
@@ -13,55 +14,49 @@ export const Breweries = ({zipCode, addFaveBreweries, faveBreweries, unFaveBrewe
     try {
       const data = await fetchData(zipCode)
       setBreweries(data)
-      console.log('breweries' , breweries)
       setNetworkError(false)
     } catch (error) {
       console.error(error)
       setNetworkError(true)
     }
   }
-  // const toggleFave = (id) => {
-  //   if (faveBreweries.includes(id)) {
-  //     setFaveBreweries(faveBreweries.filter(faveBrewID => faveBrewID !== id))
-  //   } else {
-  //     setFaveBreweries([...faveBreweries, id])
-  //   }
-  // }
 
   useEffect(() => {
     fetchBreweries()
   }, [])
-  console.log('does this do?', breweries)
 
   const breweryCards = breweries.map((brewery) => {
-    console.log('brewery', brewery)
     return (
-        <div className='brewery-container'>
-          {/* if (breweries.length === 0) */}
-          <BreweryDetails 
-            id={brewery.id}
-            key={brewery.id}
-            name={brewery.name}
-            phone={brewery.phone}
-            street={brewery.street}
-            city={brewery.city} 
-            state={brewery.state}
-            link={brewery.website_url}
-            zipCode={zipCode}
-            addFaveBreweries={addFaveBreweries}
-            faveBreweries={faveBreweries}
-            unFaveBrewery={unFaveBrewery}
-            />
-        </div>
+      <div className='brewery-container'>
+        <BreweryDetails 
+          id={brewery.id}
+          key={brewery.id}
+          name={brewery.name}
+          phone={brewery.phone}
+          street={brewery.street}
+          city={brewery.city} 
+          state={brewery.state}
+          link={brewery.website_url}
+          zipCode={zipCode}
+          addFaveBreweries={addFaveBreweries}
+          faveBreweries={faveBreweries}
+          unFaveBrewery={unFaveBrewery}
+        />
+      </div>
     )
   })
 
   return (
     <>
-    <div className='breweries-display'>
-      {breweryCards.length > 0 ? breweryCards : <Error />}
-      <h1></h1>
-    </div>
+      <div className='breweries-display'>
+        {breweryCards.length > 0 ? breweryCards : <Error />}
+        <h1></h1>
+      </div>
     </>
   )
+}
+
+Breweries.propTypes = {
+  breweries: PropTypes.array.isRequired,
+  breweries: PropTypes.arrayOf(PropTypes.object.isRequired)
 }
